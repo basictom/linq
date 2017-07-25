3143a986e13c722236e338e867edc32096c5cc5b
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-07-24 19:36:39 -0500

    completed assignment

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace linq
             };
 
             var descend = names.OrderByDescending(name => name);
-            foreach(var name in descend)
+            foreach (var name in descend)
             {
                 // Console.WriteLine(name);
             }
@@ -33,7 +33,7 @@ namespace linq
 
             var sortedNumbers = numbers.OrderBy(num => num);
 
-            foreach(var num in sortedNumbers)
+            foreach (var num in sortedNumbers)
             {
                 // Console.WriteLine(num);
             }
@@ -44,9 +44,8 @@ namespace linq
             {
                 15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
             };
-            // var finalCount = numbersTwo.Aggregate(0, (count, number) => number.Length);
-            int finalCount = numbersTwo.Sum();
-            Console.WriteLine(finalCount);
+
+            // Console.WriteLine(numbersTwo.Count);
 
             List<double> purchases = new List<double>()
             {
@@ -54,7 +53,68 @@ namespace linq
             };
 
             var total = purchases.Sum();
-            Console.WriteLine(total);
+            // Console.WriteLine(total);
+
+            Console.WriteLine("-----------");
+            /*
+                Store each number in the following List until a perfect square
+                is detected.
+
+                Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            };
+            var runSquare = wheresSquaredo.TakeWhile(number => Math.Sqrt(number) % 1 != 0);
+            foreach (var square in runSquare)
+            {
+                // Console.WriteLine(square);
+            }
+
+                        // Create some banks and store in a List
+                List<Bank> banks = new List<Bank>() {
+                    new Bank(){ Name="First Tennessee", Symbol="FTB"},
+                    new Bank(){ Name="Wells Fargo", Symbol="WF"},
+                    new Bank(){ Name="Bank of America", Symbol="BOA"},
+                    new Bank(){ Name="Citibank", Symbol="CITI"},
+                };
+
+                // Create some customers and store in a List
+                List<Customer> customers = new List<Customer>() {
+                    new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                    new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                    new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                    new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                    new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                    new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                    new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                    new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                    new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                    new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+                };
+
+                var millionaireReport = from x in customers
+                where x.Balance >= 1000000
+                group x.Name by x.Bank into z
+                select new { Name = z, Bank = z.Key };
+
+                foreach (var z in millionaireReport)
+                {
+                    Console.WriteLine($"{z.Bank}: {z.Name.Count()}");
+                }
+
         }
     }
+        public class Customer
+        {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
+        }
+        public class Bank
+        {
+            public string Symbol { get; set; }
+            public string Name { get; set; }
+        }
 }
